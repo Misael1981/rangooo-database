b2af90949db5c459d3e4d86aba6849681dc40524
Misael <misaelborges1981@gmail.com>
2026-06-27 09:25:35 -0300

    fix: prisma.config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,6 @@
+/// <reference types="node" />
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -8,6 +9,8 @@ export default defineConfig({
   },
   engine: "classic",
   datasource: {
-    url: env("DATABASE_URL"),
+    url:
+      process.env.DATABASE_URL ??
+      "postgresql://dummy:dummy@localhost:5432/dummy",
   },
 });
